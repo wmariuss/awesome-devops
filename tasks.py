3d69fcd2304e5054d5983ac1029b7081218b46ef
Kai Feldhoff <kaifeldhoff@yahoo.de>
2025-10-16 13:21:14 +0200

    secure tasks.py

--- tasks.py
+++ tasks.py
@@ -5,5 +5,5 @@ from invoke import task, call
 @task
 def build(c):
     """Build the website"""
-    c.run("cp README.md docs/index.md")
-    c.run("mkdocs build")
+    # c.run("cp README.md docs/index.md")
+    # c.run("mkdocs build")
